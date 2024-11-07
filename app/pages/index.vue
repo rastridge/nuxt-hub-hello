f64808509cd4d0a6da09fa43bd3ee99c19770ba3
rastridge <rfa@me.com>
2024-11-06 21:48:46 -0500

    change words

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,7 +1,28 @@
 <script setup lang="ts">
-const runtimeConfig = useRuntimeConfig()
-const colors = ['#f87171', '#fb923c', '#fbbf24', '#facc15', '#a3e635', '#4ade80', '#34d399', '#2dd4bf', '#22d3ee', '#38bdf8', '#60a5fa', '#818cf8', '#a78bfa', '#c084fc', '#e879f9', '#f472b6', '#fb7185']
-const color = useState('color', () => colors[Math.floor(Math.random() * colors.length)])
+const runtimeConfig = useRuntimeConfig();
+const colors = [
+  "#f87171",
+  "#fb923c",
+  "#fbbf24",
+  "#facc15",
+  "#a3e635",
+  "#4ade80",
+  "#34d399",
+  "#2dd4bf",
+  "#22d3ee",
+  "#38bdf8",
+  "#60a5fa",
+  "#818cf8",
+  "#a78bfa",
+  "#c084fc",
+  "#e879f9",
+  "#f472b6",
+  "#fb7185",
+];
+const color = useState(
+  "color",
+  () => colors[Math.floor(Math.random() * colors.length)]
+);
 </script>
 
 <template>
@@ -9,9 +30,7 @@ const color = useState('color', () => colors[Math.floor(Math.random() * colors.l
     <h1 :style="{ color }">
       {{ runtimeConfig.public.helloText }}
     </h1>
-    <NuxtLink to="/" external>
-      refresh
-    </NuxtLink>
+    <NuxtLink to="/" external> refresh me </NuxtLink>
   </div>
 </template>
 
@@ -24,7 +43,8 @@ const color = useState('color', () => colors[Math.floor(Math.random() * colors.l
   left: 50%;
   transform: translate(-50%, -50%);
   margin: 0;
-  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+    Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
 }
 h1 {
   font-size: 32px;
